722290a41a43d3e001a262e987cd4f5c785d45ed
eaharonoff <emanuel.aharonoff@gmail.com>
2016-04-29 11:38:21 -0400

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,12 @@
+def my_collect (languages)
+  new_collection = []
+  i = 0
+  while i < languages.length
+    yield languages[i]
+    new_collection << languages[i].upcase
+    i = i + 1 
+  end
+  new_collection
+end
 
 
